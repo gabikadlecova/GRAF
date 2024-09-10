ebeeefb65dca32bb4c9a7857f4917b62b3e1c26a
Gabi Kadlecová <gabi.suchoparova@gmail.com>
2024-09-10 09:40:21 +0200

    Update setup

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='graf_nas',
       version='0.1',
       description='GRAF features',
-      packages=['graf_nas']
-     )
+      packages=find_packages(),
+      include_package_data=True
+)
 
